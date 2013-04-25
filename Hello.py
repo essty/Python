b1b2686b7f19b69d1e5097d954c2e0811ebc78eb
Esther H <ehwang@cars.com>
2013-04-25 12:44:50 -0700

    added comments

--- Hello.py
+++ Hello.py
@@ -1,7 +1,9 @@
+# Wrote 3 print statements here
 print "howdy"
 print "my name is slim shady"
 print "my favorite number is", 42
 print "my favorite number is", 75
+# Wrote a new number
 print 25+34
 print 3.00/4.00
 print "hola mi amiga"
