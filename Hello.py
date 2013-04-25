a139aa6b2d4cc685e4bd8158fe9c1e13da2a51a3
Esther H <ehwang@cars.com>
2013-04-25 13:32:42 -0700

    Added two more variables

--- Hello.py
+++ Hello.py
@@ -1,7 +1,9 @@
 favorite_number = 84
+person_name = "Slim shady"
+gender = "his"
 
 print "howdy"
-print "my name is slim shady"
+print "my name is {}".format(person_name)
 print "my favorite number is {}".format(favorite_number)
 print 25+34
 print 3.00/4.00
@@ -10,7 +12,7 @@ print "hola mi amiga"
 
 
 #List everything I have my favorite number of
-print "I have {} books in my collection".format(favorite_number)
-print "I have seen {} movies".format(favorite_number)
-print "I have {} pairs of socks".format(favorite_number)
+print "{} has {} books in {} collection".format(person_name, favorite_number, gender)
+print "{} has seen {} movies".format(person_name, favorite_number)
+print "{} has {} pairs of socks".format(person_name, favorite_number)
 
