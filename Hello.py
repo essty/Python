fa0e4f8c92002e30e0d6664f7f7c38d924ddcf15
Esther H <ehwang@cars.com>
2013-04-25 13:10:02 -0700

    added comment and 98 print statements

--- Hello.py
+++ Hello.py
@@ -1,7 +1,11 @@
 print "howdy"
 print "my name is slim shady"
-print "my favorite number is", 42
-print "my favorite number is", 75
+print "my favorite number is", 98
 print 25+34
 print 3.00/4.00
 print "hola mi amiga"
+
+#List everything I have 98 things of
+print "I have 98 books in my collection"
+print "I have seen 98 movies"
+print "I have 98 pairs of socks"
