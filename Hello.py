702e998d5db217ceec46bb38c9e033aeb6828e73
Esther H <ehwang@cars.com>
2013-04-25 13:18:52 -0700

    Created new variable for favorite number

--- Hello.py
+++ Hello.py
@@ -1,11 +1,15 @@
+favorite_number = 76
+
 print "howdy"
 print "my name is slim shady"
-print "my favorite number is", 98
+print "my favorite number is {}".format(favorite_number)
 print 25+34
 print 3.00/4.00
 print "hola mi amiga"
 
-#List everything I have 98 things of
-print "I have 98 books in my collection"
-print "I have seen 98 movies"
-print "I have 98 pairs of socks"
+
+
+#List everything I have 106 things of
+print "I have 106 books in my collection"
+print "I have seen 106 movies"
+print "I have 106 pairs of socks"
