72348c884d3d3f22d31b0010964442c558cf3b62
Esther H <ehwang@cars.com>
2013-05-02 13:04:19 -0700

    added in the Import argv from sys library

--- Hello.py
+++ Hello.py
@@ -1,3 +1,7 @@
+from sys import argv
+
+print argv
+
 person_name = "Slim Shady"
 favorite_number = 42
 gender = "his"
@@ -7,28 +11,16 @@ person = (person_name, favorite_number, gender)
 
 andrew = ('Andrew', 6, 'his')
 
-print person[0]
-
-print andrew[0]
-print andrew[1]
-print andrew[2]
 
 courtney = ('Courtney', 21, 'her')
 person = courtney
 
-#print "howdy"
-#print "my name is {}".format(person_name)
-#print "my favorite number is {}".format(favorite_number)
-#print 25+34
-#print 3.00/4.00
-#print "hola mi amiga"
-
-print person[1]
+#print person[1]
 
 #List everything I have my favorite number of
-print "{} has {} books in {} collection".format(person[0], person[1], person[2])
-print "{} has seen {} of {} favorite movies".format(person[0], person[1], person[2])
-print "{} has {} pairs of socks in {} drawer".format(person[0], person[1], person[2])
+#print "{} has {} books in {} collection".format(person[0], person[1], person[2])
+#print "{} has seen {} of {} favorite movies".format(person[0], person[1], person[2])
+#print "{} has {} pairs of socks in {} drawer".format(person[0], person[1], person[2])
 
 
 
