3d3e2cf73449be12398d56e8ca43fffcbc53a741
Esther H <ehwang@cars.com>
2013-05-02 12:57:57 -0700

    Added new person variable of Courtney tuple

--- Hello.py
+++ Hello.py
@@ -1,11 +1,20 @@
-person_name = "Slim shady"
-favorite_number = 67
+person_name = "Slim Shady"
+favorite_number = 42
 gender = "his"
 
 # creating a variable, person, and assigning three traits to it
 person = (person_name, favorite_number, gender)
 
+andrew = ('Andrew', 6, 'his')
+
 print person[0]
+
+print andrew[0]
+print andrew[1]
+print andrew[2]
+
+courtney = ('Courtney', 21, 'her')
+person = courtney
 
 #print "howdy"
 #print "my name is {}".format(person_name)
@@ -14,11 +23,12 @@ print person[0]
 #print 3.00/4.00
 #print "hola mi amiga"
 
-
+print person[1]
 
 #List everything I have my favorite number of
-print "{} has {} books in {} collection".format(person[0], person[1], person[3])
+print "{} has {} books in {} collection".format(person[0], person[1], person[2])
 print "{} has seen {} of {} favorite movies".format(person[0], person[1], person[2])
 print "{} has {} pairs of socks in {} drawer".format(person[0], person[1], person[2])
+
 
 
