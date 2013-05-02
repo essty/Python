202c7a5dd22dcf4ab446895fc61b0db1e772d6d0
Esther H <ehwang@cars.com>
2013-05-02 12:43:18 -0700

    Added in new variable Person by indexes to the three sentences

--- Hello.py
+++ Hello.py
@@ -1,18 +1,24 @@
-favorite_number = 84
 person_name = "Slim shady"
+favorite_number = 67
 gender = "his"
 
-print "howdy"
-print "my name is {}".format(person_name)
-print "my favorite number is {}".format(favorite_number)
-print 25+34
-print 3.00/4.00
-print "hola mi amiga"
+# creating a variable, person, and assigning three traits to it
+person = (person_name, favorite_number, gender)
+
+print person[0]
+
+#print "howdy"
+#print "my name is {}".format(person_name)
+#print "my favorite number is {}".format(favorite_number)
+#print 25+34
+#print 3.00/4.00
+#print "hola mi amiga"
 
 
 
 #List everything I have my favorite number of
-print "{} has {} books in {} collection".format(person_name, favorite_number, gender)
-print "{} has seen {} of {} favorite movies".format(person_name, favorite_number, gender)
-print "{} has {} pairs of socks in {} drawer".format(person_name, favorite_number, gender)
+print "{} has {} books in {} collection".format(person[0], person[1], person[3])
+print "{} has seen {} of {} favorite movies".format(person[0], person[1], person[2])
+print "{} has {} pairs of socks in {} drawer".format(person[0], person[1], person[2])
+
 
