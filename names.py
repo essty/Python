06d3bd2525c3e454b0670067c5dac51c6239afa6
Esther H <ehwang@cars.com>
2013-05-09 13:10:36 -0700

    created for loops

---
+++ names.py
@@ -0,0 +1,22 @@
+teacher_names = ['andrew', 'bobby', 'esther']
+
+#for loop command. for every in
+#for teacher_name in teacher_names:
+#    python_sentence = "{} loves Python! Whoo!".format(teacher_name)
+#    print python_sentence
+
+
+#i is a temporary variable
+#for i in range(2):
+ #   print "Loop {}: I love pizza".format(i)
+
+print range(20)
+
+print python_sentence
+
+python_sentence = "{} loves Python! Whoo!".format(teacher_names[1])
+print python_sentence
+
+python_sentence = "{} loves Python! Whoo!".format(teacher_names[2])
+print python_sentence
+
