55c030dc967baf773c7c879187ce44e935c78969
Esther H <ehwang@cars.com>
2013-05-09 11:44:06 -0700

    named the pizza variable and printed with the variables inserted into the format function

---
+++ pizza.py
@@ -0,0 +1,5 @@
+pizza = ('mushroom', 8, 'thin crust')
+topping, number_slices, crust = pizza
+
+#Don't change the actual text of sentences.
+print "My favorite pizza topping is {}. I can eat {} slices of that! But only on a {} crust".format(pizza[0],pizza[1],pizza[2])
