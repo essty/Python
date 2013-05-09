489f71caa5f95ef8916bf9dc696c0674a973157b
Esther H <ehwang@cars.com>
2013-05-09 13:17:12 -0700

    Adding 'in' sentence

--- names.py
+++ names.py
@@ -1,22 +1,3 @@
-teacher_names = ['andrew', 'bobby', 'esther']
+teacher_names = ['andrew', 'bobby', 'esther', 'travis']
 
-#for loop command. for every in
-#for teacher_name in teacher_names:
-#    python_sentence = "{} loves Python! Whoo!".format(teacher_name)
-#    print python_sentence
-
-
-#i is a temporary variable
-#for i in range(2):
- #   print "Loop {}: I love pizza".format(i)
-
-print range(20)
-
-print python_sentence
-
-python_sentence = "{} loves Python! Whoo!".format(teacher_names[1])
-print python_sentence
-
-python_sentence = "{} loves Python! Whoo!".format(teacher_names[2])
-print python_sentence
-
+print 'Andrew' not in teacher_names
