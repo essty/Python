4d8a9bb4429217504b7fa7b9c97cea2d6eca000c
Esther H <ehwang@cars.com>
2013-05-16 11:10:32 -0700

    added a for loop within another for loop to print out the ingredients of each list

---
+++ more_ingredients.py
@@ -0,0 +1,10 @@
+burger = ['bun', 'lettuce', 'cheese', 'patty', 'tomato slices']
+spaghetti = ['spaghetti pasta', 'garlic', 'tomato sauce']
+pancake = ['flour', 'egg', 'sugar', 'butter']
+
+dishes = [burger, spaghetti, pancake]
+
+
+for dish in dishes:
+    for ingredients in dish:
+	    print ingredients
