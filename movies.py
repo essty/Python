3f3c4ef96bbba739432067d3d24092d445691cee
Esther H <ehwang@cars.com>
2013-05-15 17:46:54 -0700

    added in a list named movies

--- movies.py
+++ movies.py
@@ -2,3 +2,5 @@ movie_1 = ('Iron Man 3', 78)
 movie_2 = ('The Great Gatsby', 48)
 movie_3 = ('Star Trek Into Darkness', 90)
 movie_4 = ('Oblivion', 56)
+
+movies = [movie_1, movie_2, movie_3, movie_4]
