836dc619e82e04fc6b858b7c36d9ed06da41ee05
Esther H <ehwang@cars.com>
2013-05-16 10:17:13 -0700

    added for loop with 'scores' variable to produce the output of movies with their respective ratings

--- movies.py
+++ movies.py
@@ -4,3 +4,11 @@ movie_3 = ('Star Trek Into Darkness', 90)
 movie_4 = ('Oblivion', 56)
 
 movies = [movie_1, movie_2, movie_3, movie_4]
+
+
+
+
+
+for scores in movies:
+    movie_sentence = "{} is rated {} out of 100".format(scores[0],scores[1])
+    print movie_sentence
