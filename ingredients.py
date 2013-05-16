c55e04a1ec036e721342f409189a033b8a86d479
Esther H <ehwang@cars.com>
2013-05-16 10:42:41 -0700

    added in a for loop to repeat ingredients in burger

---
+++ ingredients.py
@@ -0,0 +1,7 @@
+burger = ['bun', 'lettuce', 'cheese', 'patty', 'tomato slices']
+spaghetti = ['spaghetti pasta', 'garlic', 'tomato sauce']
+pancake = ['flour', 'egg', 'sugar', 'butter']
+
+
+for ingredients in burger:
+    print ingredients
