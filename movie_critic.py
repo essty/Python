49c72950186136a5ea5e22c991e09430452e16a0
Esther H <ehwang@cars.com>
2013-05-22 17:15:58 -0700

    added in a for loop with if statement and elif statement and else option. also included newline breaks

--- movie_critic.py
+++ movie_critic.py
@@ -2,4 +2,18 @@ recent_movies = [('Iron Man 3', 78),
                  ('The Great Gatsby', 48),
                  ('Star Trek Into Darkness', 90),
                  ('Oblivion', 56)]
-Print "/n"
+print "\n"
+
+for movie, rating in recent_movies:
+    if rating > int('75'):
+        print "I watched {} the other day.".format(movie)
+        print "It was awesome!!"
+        print "\n"
+    elif rating < int('50'):
+        print "I watched {} the other day.".format(movie)
+        print "This movie sucked!".format(movie)
+        print "\n"
+    else:
+        print "I watched {} the other day.".format(movie)
+        print "It was okay."
+        print "\n"
