38a6f0394ce037d8d85faad45292669ebadb6c62
Esther H <ehwang@cars.com>
2013-05-22 18:15:52 -0700

    corrected some errors in the print statement describing how the movie was

--- movie_critic.py
+++ movie_critic.py
@@ -17,13 +17,13 @@ for i in recent_movies:
                 print "{} was pretty good in it, though.".format(actor)
         print "\n"
     elif i[1] < int('50'):
-        print "I watched {0} the other day.\nThis movie was awesome!!\nRotten Tomatoes agrees: they gave {0} a score of {1} out of 100".format(i[0],i[1])
+        print "I watched {0} the other day.\nThis movie sucked!\nRotten Tomatoes agrees: they gave {0} a score of {1} out of 100".format(i[0],i[1])
         for actor, movie_name in movie_stars:
             if i[0] in movie_name:
                 print "{} was pretty good in it, though.".format(actor)
         print "\n"
     else:
-        print "I watched {0} the other day.\nThis movie was awesome!!\nRotten Tomatoes agrees: they gave {0} a score of {1} out of 100".format(i[0],i[1])
+        print "I watched {0} the other day.\nIt was okay.\nRotten Tomatoes agrees: they gave {0} a score of {1} out of 100".format(i[0],i[1])
         for actor, movie_name in movie_stars:
             if i[0] in movie_name:
                 print "{} was pretty good in it, though.".format(actor)
